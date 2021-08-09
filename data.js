251e1a206a7503f7ff9c0c8ed35bf2f1ac82db72
Henry Gordon <hwgordon247@gmail.com>
2021-08-09 23:03:56 +0100

    importing all images

--- data.js
+++ data.js
@@ -42,6 +42,61 @@ const divingOttersSculptureInfo = {
     spin: "diving-otters/diving-otters-spin"
 }
 
+const giraffeBustSculptureInfo = {
+    id: '#giraffe-bust-sculpture',
+    name: "Giraffe Bust",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/giraffe-bust/giraffe-bust-1.jpg", "img/sculptures/giraffe-bust/giraffe-bust-2.jpg", "img/sculptures/giraffe-bust/giraffe-bust-3.jpg", "img/old/giraffe/giraffe-compress.jpg"],
+    spin: "giraffe-bust/giraffe-bust-spin"
+}
+
+const giraffeStudySculptureInfo = {
+    id: '#giraffe-study-sculpture',
+    name: "Giraffe Bust",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/giraffe-study/giraffe-study-1.jpg"],
+    spin: "giraffe-study/giraffe-study-spin"
+}
+
+const guineaFowlSculptureInfo = {
+    id: '#guinea-fowl-sculpture',
+    name: "Giraffe Bust",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/guinea-fowl/guinea-fowl-1.jpg", "img/sculptures/guinea-fowl/guinea-fowl-2.jpg", "img/sculptures/guinea-fowl/guinea-fowl-3.jpg"],
+    spin: "guinea-fowl/guinea-fowl-spin"
+}
+
+const kuduHerdSculptureInfo = {
+    id: '#kudu-herd-sculpture',
+    name: "Giraffe Bust",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/kudu-herd/kudu-herd-1.jpg", "img/sculptures/kudu-herd/kudu-herd-2.jpg"],
+    spin: "kudu-herd/kudu-herd-spin"
+}
+
+const langurMonkeySculptureInfo = {
+    id: '#langur-monkey-sculpture',
+    name: "Giraffe Bust",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/langur-monkey/langur-monkey-1.jpg"],
+    spin: "langur-monkey/langur-monkey-spin"
+}
+
 const malayanTapirSculptureInfo = {
     id: '#malayan-tapir-sculpture',
     name: "Malayan Tapir",
@@ -53,15 +108,48 @@ const malayanTapirSculptureInfo = {
     spin: "malayan-tapir/malayan-tapir-spin"
 }
 
-const giraffeBustSculptureInfo = {
-    id: '#giraffe-bust-sculpture',
-    name: "Giraffe Bust",
+const ostrichStudySculptureInfo = {
+    id: '#ostrich-study-sculpture',
+    name: "Malayan Tapir",
     description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
-    images: ["img/sculptures/giraffe-bust/giraffe-bust-1.jpg", "img/sculptures/giraffe-bust/giraffe-bust-2.jpg", "img/sculptures/giraffe-bust/giraffe-bust-3.jpg", "img/old/giraffe/giraffe-compress.jpg"],
-    spin: "giraffe-bust/giraffe-bust-spin"
+    dimensions: "23cm high x 42 x 20",
+    date: "June 2020",
+    price: "£5,000",
+    images: ["img/sculptures/ostrich-study/ostrich-study-1.jpg"],
+    spin: "ostrich-study/ostrich-study-spin"
+}
+
+const peregrineFalconSculptureInfo = {
+    id: '#peregrine-falcon-sculpture',
+    name: "Malayan Tapir",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "23cm high x 42 x 20",
+    date: "June 2020",
+    price: "£5,000",
+    images: ["img/sculptures/peregrine-falcon/peregrine-falcon-1.jpg", "img/sculptures/peregrine-falcon/peregrine-falcon-2.jpg"],
+    spin: "peregrine-falcon/peregrine-falcon-spin"
+}
+
+const prancingRoeDeerSculptureInfo = {
+    id: '#prancing-roe-deer-sculpture',
+    name: "Malayan Tapir",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "23cm high x 42 x 20",
+    date: "June 2020",
+    price: "£5,000",
+    images: ["img/sculptures/prancing-roe-deer/prancing-roe-deer-1.jpg"],
+    spin: "prancing-roe-deer/prancing-roe-deer-spin"
+}
+
+const redSquirrelsSculptureInfo = {
+    id: '#red-squirrels-sculpture',
+    name: "Malayan Tapir",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "23cm high x 42 x 20",
+    date: "June 2020",
+    price: "£5,000",
+    images: ["img/sculptures/red-squirrels/red-squirrels-1.jpg"],
+    spin: "red-squirrels/red-squirrels-spin"
 }
 
 const runningHareSculptureInfo = {
@@ -75,12 +163,152 @@ const runningHareSculptureInfo = {
     spin: "running-hare/running-hare-spin"
 }
 
+const sableAntelopeStudySculptureInfo = {
+    id: '#sable-antelope-study-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/sable-antelope-study/sable-antelope-study-1.jpg"],
+    spin: "sable-antelope-study/sable-antelope-study-spin"
+}
+
+const seaLionSculptureInfo = {
+    id: '#sea-lion-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/sea-lion/sea-lion-1.jpg", "img/sculptures/sea-lion/sea-lion-2.jpg", "img/sculptures/sea-lion/sea-lion-3.jpg"],
+    spin: "sea-lion/sea-lion-spin"
+}
+
+const southernWhiteRhinoSculptureInfo = {
+    id: '#southern-white-rhino-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/southern-white-rhino/southern-white-rhino-1.jpg", "img/sculptures/southern-white-rhino/southern-white-rhino-2.jpg", "img/sculptures/southern-white-rhino/southern-white-rhino-3.jpg"],
+    spin: "southern-white-rhino/southern-white-rhino-spin"
+}
+
+const stalkingLeopardSculptureInfo = {
+    id: '#stalking-leopard-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/stalking-leopard/stalking-leopard-1.jpg"],
+    spin: "stalking-leopard/stalking-leopard-spin"
+}
+
+const swimmingOttersSculptureInfo = {
+    id: '#swimming-otters-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/swimming-otters/swimming-otters-1.jpg"],
+    spin: "swimming-otters/swimming-otters-spin"
+}
+
+const thoroughbredLifeStudySculptureInfo = {
+    id: '#thoroughbred-life-study-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/thoroughbred-life-study/thoroughbred-life-study-1.jpg"],
+    spin: "thoroughbred-life-study/thoroughbred-life-study-spin"
+}
+
+const toadSculptureInfo = {
+    id: '#toad-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/toad/toad-1.jpg", "img/sculptures/toad/toad-2.jpg", "img/sculptures/toad/toad-3.jpg"],
+    spin: "toad/toad-spin"
+}
+
+const treeFrogSculptureInfo = {
+    id: '#tree-frog-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/tree-frog/tree-frog-1.jpg", "img/sculptures/tree-frog/tree-frog-2.jpg"],
+    spin: "tree-frog/tree-frog-spin"
+}
+
+const walkingHorseSculptureInfo = {
+    id: '#walking-horse-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/walking-horse/walking-horse-1.jpg", "img/sculptures/walking-horse/walking-horse-2.jpg", "img/sculptures/walking-horse/walking-horse-3.jpg"],
+    spin: "walking-horse/walking-horse-spin"
+}
+
+const warthogStudySculptureInfo = {
+    id: '#warthog-study-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/warthog-study/warthog-study-1.jpg"],
+    spin: "warthog-study/warthog-study-spin"
+}
+
+const whiteRhinoStudySculptureInfo = {
+    id: '#white-rhino-study-sculpture',
+    name: "Running Hare",
+    description: "Bronze, signed and numbered limited edition of 12",
+    dimensions: "78cm high x 35 x 35",
+    date: "November 2019",
+    price: "£9,500",
+    images: ["img/sculptures/white-rhino-study/white-rhino-study-1.jpg"],
+    spin: "white-rhino-study/white-rhino-study-spin"
+}
+
 const allSculptures = [
     barnSwallowSculptureInfo,
     blackbirdSculptureInfo,
     chickenSculptureInfo,
     divingOttersSculptureInfo,
-    malayanTapirSculptureInfo,
     giraffeBustSculptureInfo,
+    giraffeStudySculptureInfo,
+    guineaFowlSculptureInfo,
+    kuduHerdSculptureInfo,
+    langurMonkeySculptureInfo,
+    malayanTapirSculptureInfo,
+    ostrichStudySculptureInfo,
+    peregrineFalconSculptureInfo,
+    prancingRoeDeerSculptureInfo,
+    redSquirrelsSculptureInfo,
+    sableAntelopeStudySculptureInfo,
+    seaLionSculptureInfo,
+    southernWhiteRhinoSculptureInfo,
+    stalkingLeopardSculptureInfo,
+    swimmingOttersSculptureInfo,
+    thoroughbredLifeStudySculptureInfo,
+    toadSculptureInfo,
+    treeFrogSculptureInfo,
+    walkingHorseSculptureInfo,
+    warthogStudySculptureInfo,
+    whiteRhinoStudySculptureInfo,
     runningHareSculptureInfo
 ];
