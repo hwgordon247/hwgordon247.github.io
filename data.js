1c4c5edda7521c7f462e211c1a51ff5ee176fb7c
Henry Gordon <hwgordon247@gmail.com>
2021-08-31 23:12:52 +0100

    teaskdhtare;klgt

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const barnSwallowSculptureInfo = {
     id: '#barn-swallow-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Barn Swallow",
+    description: "Bronze, Limited Edition of 12",
+    dimensions: "30cm high x 34 x 14",
+    date: "February 2016",
+    price: "£3,600 inc. VAT",
     images: ["img/sculptures/barn-swallow/barn-swallow-1.jpg"],
     spin: "barn-swallow/barn-swallow-spin"
 }
@@ -22,22 +22,22 @@ const blackbirdSculptureInfo = {
 
 const chickenSculptureInfo = {
     id: '#chicken-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Chicken II",
+    description: "Bronze, limited edition of 12",
+    dimensions: "45cm high x 30 x 20",
+    date: "July 2018",
+    price: "£5,800 inc. VAT",
     images: ["img/sculptures/chicken/chicken-1.jpg"],
     spin: "chicken/chicken-spin"
 }
 
 const divingOttersSculptureInfo = {
     id: '#diving-otters-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Diving Otters",
+    description: "Bronze, signed and numbered limited edition of 9",
+    dimensions: "160cm high x 100 x 90",
+    date: "March 2021",
+    price: "£35,000 inc. Vat",
     images: ["img/sculptures/diving-otters/diving-otters-1.jpg", "img/sculptures/diving-otters/diving-otters-2.jpg", "img/sculptures/diving-otters/diving-otters-3.jpg", "img/sculptures/diving-otters/diving-otters-4.jpg", "img/sculptures/diving-otters/diving-otters-5.jpg"],
     spin: "diving-otters/diving-otters-spin"
 }
@@ -48,51 +48,51 @@ const giraffeBustSculptureInfo = {
     description: "Bronze, signed and numbered limited edition of 12",
     dimensions: "78cm high x 35 x 35",
     date: "November 2019",
-    price: "£9,500",
+    price: "£9,900 inc. VAT",
     images: ["img/sculptures/giraffe-bust/giraffe-bust-1.jpg", "img/sculptures/giraffe-bust/giraffe-bust-2.jpg", "img/sculptures/giraffe-bust/giraffe-bust-3.jpg", "img/old/giraffe/giraffe-compress.jpg"],
     spin: "giraffe-bust/giraffe-bust-spin"
 }
 
 const giraffeStudySculptureInfo = {
     id: '#giraffe-study-sculpture',
-    name: "Giraffe Bust",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Giraffe Study",
+    description: "Bronze, limited edition of 12",
+    dimensions: "25cm high x 20 x 10",
+    date: "January 2019",
+    price: "£3,000 inc. VAT",
     images: ["img/sculptures/giraffe-study/giraffe-study-1.jpg"],
     spin: "giraffe-study/giraffe-study-spin"
 }
 
 const guineaFowlSculptureInfo = {
     id: '#guinea-fowl-sculpture',
-    name: "Giraffe Bust",
+    name: "Guinea Fowl",
     description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    dimensions: "46cm high x 30 x 18",
+    date: "March 2020",
+    price: "£6,200 inc. VAT",
     images: ["img/sculptures/guinea-fowl/guinea-fowl-1.jpg", "img/sculptures/guinea-fowl/guinea-fowl-2.jpg", "img/sculptures/guinea-fowl/guinea-fowl-3.jpg"],
     spin: "guinea-fowl/guinea-fowl-spin"
 }
 
 const kuduHerdSculptureInfo = {
     id: '#kudu-herd-sculpture',
-    name: "Giraffe Bust",
+    name: "Kudu Herd",
     description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    dimensions: "50cm high x 80 x 40",
+    date: "December 2019",
+    price: "£19,800 inc. VAT",
     images: ["img/sculptures/kudu-herd/kudu-herd-1.jpg", "img/sculptures/kudu-herd/kudu-herd-2.jpg"],
     spin: "kudu-herd/kudu-herd-spin"
 }
 
 const langurMonkeySculptureInfo = {
     id: '#langur-monkey-sculpture',
-    name: "Giraffe Bust",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Langur Monkey",
+    description: "Bronze, Limited Edition of 12",
+    dimensions: "23cm high x 28 x 15",
+    date: "October 2018",
+    price: "£3,000 inc. VAT",
     images: ["img/sculptures/langur-monkey/langur-monkey-1.jpg"],
     spin: "langur-monkey/langur-monkey-spin"
 }
@@ -103,51 +103,51 @@ const malayanTapirSculptureInfo = {
     description: "Bronze, signed and numbered limited edition of 12",
     dimensions: "23cm high x 42 x 20",
     date: "June 2020",
-    price: "£5,000",
+    price: "£6,000 inc. VAT",
     images: ["img/sculptures/malayan-tapir/malayan-tapir-1.jpg", "img/sculptures/malayan-tapir/malayan-tapir-2.jpg", "img/sculptures/malayan-tapir/malayan-tapir-3.jpg"],
     spin: "malayan-tapir/malayan-tapir-spin"
 }
 
 const ostrichStudySculptureInfo = {
     id: '#ostrich-study-sculpture',
-    name: "Malayan Tapir",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "23cm high x 42 x 20",
-    date: "June 2020",
-    price: "£5,000",
+    name: "Ostrich Study",
+    description: "Bronze, limited edition of 12",
+    dimensions: "12cm high x 8 x 10 ",
+    date: "January 2019",
+    price: "£1,800 inc. VAT",
     images: ["img/sculptures/ostrich-study/ostrich-study-1.jpg"],
     spin: "ostrich-study/ostrich-study-spin"
 }
 
 const peregrineFalconSculptureInfo = {
     id: '#peregrine-falcon-sculpture',
-    name: "Malayan Tapir",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "23cm high x 42 x 20",
-    date: "June 2020",
-    price: "£5,000",
+    name: "Peregrine Falcon",
+    description: "Bronze, Limited Edition of 12",
+    dimensions: "Life size - 130cm high x 45 x 45",
+    date: "August 2017",
+    price: "£10,200 inc. VAT",
     images: ["img/sculptures/peregrine-falcon/peregrine-falcon-1.jpg", "img/sculptures/peregrine-falcon/peregrine-falcon-2.jpg"],
     spin: "peregrine-falcon/peregrine-falcon-spin"
 }
 
 const prancingRoeDeerSculptureInfo = {
     id: '#prancing-roe-deer-sculpture',
-    name: "Malayan Tapir",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "23cm high x 42 x 20",
-    date: "June 2020",
-    price: "£5,000",
+    name: "Prancing Roe Deer",
+    description: "Bronze, Limited Edition of 12",
+    dimensions: "84cm high x 94 x 25",
+    date: "April 2018",
+    price: "£19,200 inc. VAT",
     images: ["img/sculptures/prancing-roe-deer/prancing-roe-deer-1.jpg"],
     spin: "prancing-roe-deer/prancing-roe-deer-spin"
 }
 
 const redSquirrelsSculptureInfo = {
     id: '#red-squirrels-sculpture',
-    name: "Malayan Tapir",
+    name: "Red Squirrel Pair",
     description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "23cm high x 42 x 20",
-    date: "June 2020",
-    price: "£5,000",
+    dimensions: "16cm high x 28 x 20 each",
+    date: "May 2020",
+    price: "£3,800 each inc. VAT, £7,600 pair inc. VAT",
     images: ["img/sculptures/red-squirrels/red-squirrels-1.jpg"],
     spin: "red-squirrels/red-squirrels-spin"
 }
@@ -155,175 +155,175 @@ const redSquirrelsSculptureInfo = {
 const runningHareSculptureInfo = {
     id: '#running-hare-sculpture',
     name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    description: "Bronze signed and numbered limited edition of 12",
+    dimensions: "30cm high x 55 x 18",
+    date: "August 2020",
+    price: "£6,900 inc. VAT",
     images: ["img/sculptures/running-hare/running-hare-1.jpg", "img/sculptures/running-hare/running-hare-2.jpg", "img/sculptures/running-hare/running-hare-3.jpg"],
     spin: "running-hare/running-hare-spin"
 }
 
 const sableAntelopeStudySculptureInfo = {
     id: '#sable-antelope-study-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Sable Antelope Study",
+    description: "Bronze, limited edition of 12",
+    dimensions: "28cm high x 23 x 12",
+    date: "January 2019",
+    price: "£4,200 inc. VAT",
     images: ["img/sculptures/sable-antelope-study/sable-antelope-study-1.jpg"],
     spin: "sable-antelope-study/sable-antelope-study-spin"
 }
 
 const seaLionSculptureInfo = {
     id: '#sea-lion-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
-    images: ["img/sculptures/sea-lion/sea-lion-1.jpg", "img/sculptures/sea-lion/sea-lion-2.jpg", "img/sculptures/sea-lion/sea-lion-3.jpg"],
+    name: "Sea Lion",
+    description: "Bronze, signed and numbered limited edition of 25",
+    dimensions: "17cm high x 22 x 23",
+    date: "July 2020",
+    price: "£2,200 inc. VAT",
+    images: ["img/sculptures/sea-lion/sea-lion-1.jpg", "img/sculptures/sea-lion/sea-lion-2.jpg"],
     spin: "sea-lion/sea-lion-spin"
 }
 
 const southernWhiteRhinoSculptureInfo = {
     id: '#southern-white-rhino-sculpture',
-    name: "Running Hare",
+    name: "Southern White Rhino",
     description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    dimensions: "30cm high x 64 x 25",
+    date: "August 2019",
+    price: "£7,200 inc. VAT",
     images: ["img/sculptures/southern-white-rhino/southern-white-rhino-1.jpg", "img/sculptures/southern-white-rhino/southern-white-rhino-2.jpg", "img/sculptures/southern-white-rhino/southern-white-rhino-3.jpg"],
     spin: "southern-white-rhino/southern-white-rhino-spin"
 }
 
 const stalkingLeopardSculptureInfo = {
     id: '#stalking-leopard-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Leopard Stalking",
+    description: "Bronze, Limited Edition of 12",
+    dimensions: "30cm high x 60 x 15",
+    date: "April 2017",
+    price: "£6,000 inc. VAT",
     images: ["img/sculptures/stalking-leopard/stalking-leopard-1.jpg"],
     spin: "stalking-leopard/stalking-leopard-spin"
 }
 
 const swimmingOttersSculptureInfo = {
     id: '#swimming-otters-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Swimming Otters",
+    description: "Bronze, Limited Edition of 25",
+    dimensions: "65cm high x 20 x 20",
+    date: "January 2017",
+    price: "£8,400 inc. VAT",
     images: ["img/sculptures/swimming-otters/swimming-otters-1.jpg"],
     spin: "swimming-otters/swimming-otters-spin"
 }
 
 const thoroughbredLifeStudySculptureInfo = {
     id: '#thoroughbred-life-study-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Thoroughbred Life Study",
+    description: "Bronze, Limited Edition of 12",
+    dimensions: "20cm high x 25 x 10",
+    date: "March 2018",
+    price: "£3,600 inc. VAT",
     images: ["img/sculptures/thoroughbred-life-study/thoroughbred-life-study-1.jpg"],
     spin: "thoroughbred-life-study/thoroughbred-life-study-spin"
 }
 
 const toadSculptureInfo = {
     id: '#toad-sculpture',
-    name: "Running Hare",
+    name: "Toad",
     description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    dimensions: "10cm high x 18 x 8",
+    date: "July 2020",
+    price: "£2,100 inc. VAT",
     images: ["img/sculptures/toad/toad-1.jpg", "img/sculptures/toad/toad-2.jpg", "img/sculptures/toad/toad-3.jpg"],
     spin: "toad/toad-spin"
 }
 
 const treeFrogSculptureInfo = {
     id: '#tree-frog-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Tree frog",
+    description: "Bronze, Limited Edition of 12",
+    dimensions: "55cm high x 15 x 12",
+    date: "January 2018",
+    price: "£4,200 inc. VAT",
     images: ["img/sculptures/tree-frog/tree-frog-1.jpg", "img/sculptures/tree-frog/tree-frog-2.jpg"],
     spin: "tree-frog/tree-frog-spin"
 }
 
 const walkingHorseSculptureInfo = {
     id: '#walking-horse-sculpture',
-    name: "Running Hare",
+    name: "Walking Horse",
     description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    dimensions: "34cm high x 60 x 22",
+    date: "April 2020",
+    price: "£6,800 inc. VAT",
     images: ["img/sculptures/walking-horse/walking-horse-1.jpg", "img/sculptures/walking-horse/walking-horse-2.jpg", "img/sculptures/walking-horse/walking-horse-3.jpg"],
     spin: "walking-horse/walking-horse-spin"
 }
 
 const warthogStudySculptureInfo = {
     id: '#warthog-study-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Warthog Study",
+    description: "Bronze, limited edition of 12",
+    dimensions: "15cm high x 25 x 8",
+    date: "January 2019",
+    price: "£3,600 inc. VAT",
     images: ["img/sculptures/warthog-study/warthog-study-1.jpg"],
     spin: "warthog-study/warthog-study-spin"
 }
 
 const whiteRhinoStudySculptureInfo = {
     id: '#white-rhino-study-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "White Rhino Study",
+    description: "Bronze, limited edition of 12",
+    dimensions: "18cm high x 25 x 12",
+    date: "January 2019",
+    price: "£3,600 inc. VAT",
     images: ["img/sculptures/white-rhino-study/white-rhino-study-1.jpg"],
     spin: "white-rhino-study/white-rhino-study-spin"
 }
 
 const ducklingSculptureInfo = {
     id: '#duckling-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Duckling",
+    description: "Bronze, signed and numbered limited edition of 25",
+    dimensions: "18cm high x 16 x 10",
+    date: "July 2020",
+    price: "£2,200 inc. VAT",
     images: ["img/sculptures/duckling/duckling-1.jpg", "img/sculptures/duckling/duckling-2.jpg", "img/sculptures/duckling/duckling-3.jpg"],
     spin: "duckling/duckling-spin"
 }
 
 const foxPairInfo = {
     id: '#fox-pair-sculpture',
-    name: "Running Hare",
+    name: "Fox Pair",
     description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    dimensions: "40cm high x 50 x 30 each",
+    date: "January 2020",
+    price: "£24,000 inc. VAT",
     images: ["img/sculptures/fox-pair/fox-pair-1.jpg", "img/sculptures/fox-pair/fox-pair-2.jpg", "img/sculptures/fox-pair/fox-pair-3.jpg"],
     spin: "fox-pair/fox-pair-spin"
 }
 
 const fightOrFlightInfo = {
     id: '#fight-or-flight-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Fight or flight",
+    description: "Bronze, Limited Edition of 12",
+    dimensions: "54cm high x 28 x 18",
+    date: "August 2017",
+    price: "£6,000 inc. VAT",
     images: ["img/sculptures/fight-or-flight/fight-or-flight-1.jpg"],
     spin: "fight-or-flight/fight-or-flight-spin"
 }
 
 const gallopingHorseInfo = {
     id: '#galloping-horse-sculpture',
-    name: "Running Hare",
-    description: "Bronze, signed and numbered limited edition of 12",
-    dimensions: "78cm high x 35 x 35",
-    date: "November 2019",
-    price: "£9,500",
+    name: "Galloping Horse",
+    description: "Bronze, Limited Edition of 12",
+    dimensions: "42cm high x 48 x 14",
+    date: "April 2016",
+    price: "£6,000 inc. VAT",
     images: ["img/sculptures/galloping-horse/galloping-horse-1.jpg"],
     spin: "galloping-horse/galloping-horse-spin"
 }
